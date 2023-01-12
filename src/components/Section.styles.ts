82017b07f3ace2d385699edc94fa0011162212be
Mari Vaypan <avemari98@gmail.com>
2023-01-13 01:11:00 +0300

    add size and weigth to headlines

--- src/components/Section.styles.ts
+++ src/components/Section.styles.ts
@@ -5,4 +5,20 @@ export const Wrapper = styled.section`
     background: #FFFFFF;
     border-radius: 17px;
     margin-bottom: 25px;
+
+    h1 {
+        font-weight: 700;
+        font-size: 30px;
+    }
+
+    h2 {
+        font-weight: 600;
+        font-size: 28px;
+    }
+
+    h3 {
+        font-weight: 500;
+        font-size: 18px;
+    }
+
 `
