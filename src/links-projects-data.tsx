40b86cb3204f39760fb83c3f44e1ae8b3ec87ec8
Mari Vaypan <avemari98@gmail.com>
2023-01-12 17:25:00 +0300

    create projects data

---
+++ src/links-projects-data.tsx
@@ -0,0 +1,13 @@
+let projects:{title: string, url: string}[];
+
+export default projects = [
+    {
+        title: 'Goals',
+        url: 'https://your-goal-avemari.netlify.app'
+    },
+    {
+        title: 'Todo List',
+        url: 'https://todo-application-avemari.netlify.app/'
+    }
+
+]
