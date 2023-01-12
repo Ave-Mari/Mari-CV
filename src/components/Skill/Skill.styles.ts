a2b4354c325e9c59ed926ef52e6e94cdcaed3d4c
Mari Vaypan <avemari98@gmail.com>
2023-01-12 17:43:34 +0300

    fix skills display

--- src/components/Skill/Skill.styles.ts
+++ src/components/Skill/Skill.styles.ts
@@ -4,7 +4,7 @@ export const Element = styled.div`
         padding: 7px 24px;
         background: #7C3AD2;
         border-radius: 20px;
-        display: inline;
+        display: inline-block;
         color: #fff;
 
 `
