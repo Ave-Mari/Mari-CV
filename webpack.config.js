0670d1d874360cf6e5a9f5890d7de6e47cc647f5
Mari Vaypan <avemari98@gmail.com>
2023-01-12 17:10:15 +0300

    add css loader to project

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ const config = {
         ]
       },
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
