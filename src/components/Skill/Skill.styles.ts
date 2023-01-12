80567f558efc95c6acac0e983d3aabad6538fc5f
Mari Vaypan <avemari98@gmail.com>
2023-01-13 01:04:18 +0300

    fix skills elements size

--- src/components/Skill/Skill.styles.ts
+++ src/components/Skill/Skill.styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 export const Element = styled.div`
-        padding: 7px 24px;
+        padding: 5px 15px;
+        font-size: 13px;
         background: #7C3AD2;
         border-radius: 20px;
         display: inline-block;
         color: #fff;
+        margin-right: 3px;
 
 `
