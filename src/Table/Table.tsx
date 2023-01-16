607577e4d8fbc0663775c6c7bd7bb1e9e5e49749
Mari Vaypan <avemari98@gmail.com>
2023-01-16 22:35:16 +0300

    create basic table layout

---
+++ src/Table/Table.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+const Table: React.FC = () => {
+  return (
+    <table>
+    <tr>
+      <td>E-mail:</td>
+      <td>avemari98@gmail.com</td>
+    </tr>
+    <tr>
+      <td>Telegram:</td>
+      <td>@marifog</td>
+    </tr>
+    <tr>
+      <td>Github:</td>
+      <td>https://github.com/Ave-Mari</td>
+    </tr>
+  </table>
+  )
+}
+
+
+export default Table;
