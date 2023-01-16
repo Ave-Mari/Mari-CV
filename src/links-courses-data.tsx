5429469d6fb23dde60121bd8205a672a6b8bc7b9
Mari Vaypan <avemari98@gmail.com>
2023-01-16 21:13:53 +0300

    add courses data

---
+++ src/links-courses-data.tsx
@@ -0,0 +1,16 @@
+type courses = {title: string, url: string}[];
+let courses = [
+    {
+        title: 'Loftschool — Web-разработка для начинающих',
+        url: 'https://loftschool.com/diploma/GJ1604499868/ru/pdf'
+    },
+    {
+        title: 'Loftschool — комплексное обучение JavaScript',
+        url: 'https://loftschool.com/diploma/FB1607986487/ru/pdf'
+    }
+];
+
+
+
+
+export default courses;
