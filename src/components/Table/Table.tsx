88856b3b05c0d32ff559805544105465dfeeb378
Mari Vaypan <avemari98@gmail.com>
2023-01-19 01:29:57 +0300

    add function to copy contact information

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,21 +1,38 @@
 import * as React from 'react';
+import {useState} from 'react';
 //styles
-import { Wrapper } from './Table.styles'
+import { Wrapper } from './Table.styles';
+//types
+type clickToCopy = (...args: any) => void;
 
 const Table: React.FC = () => {
+
+  //const [email, setEmail] = useState<string>('avemari98@gmail.com');
+
+  const clickToCopy: clickToCopy = (e) => {
+    console.log(e.target.value);
+    navigator.clipboard.writeText(e.target.value);
+  };
   return (
     <Wrapper>
     <tr> 
       <th scope='row'>E-mail:</th>
-      <td>avemari98@gmail.com</td>
+      <td>
+        <button value="avemari98@gmail.com" onClick={e => clickToCopy(e)}>avemari98@gmail.com</button>
+      </td>
     </tr>
     <tr>
     <th scope='row'>Telegram:</th>
-      <td>@marifog</td>
+      <td>
+      <button value="@marifog" onClick={e => clickToCopy(e)}>@marifog</button>
+      </td>
     </tr>
     <tr>
     <th scope='row'>Github:</th>
-      <td>https://github.com/Ave-Mari</td>
+      <td>
+        <a href='https://github.com/Ave-Mari' target="_blank">Ave-Mari</a>
+        
+        </td>
     </tr>
   </Wrapper>
   )
