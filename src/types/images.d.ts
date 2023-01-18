9900fa35ebac0ae9ef3e596adff3596bd50390e9
Mari Vaypan <avemari98@gmail.com>
2023-01-19 02:07:23 +0300

    add svg type

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,2 +1,8 @@
 declare module '*.jpg';
 declare module '*.jpeg';
+
+
+declare module "*.svg" {
+    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    export default content;
+  }
