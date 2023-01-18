8d8c66775f2c50be951042943073571ce85b8b1d
Mari Vaypan <avemari98@gmail.com>
2023-01-19 01:38:55 +0300

    add styles to  table links

--- src/components/Table/Table.styles.ts
+++ src/components/Table/Table.styles.ts
@@ -9,5 +9,31 @@ export const Wrapper = styled.table`
     td {
         width: 10%;
         text-align: left;
+
+        button {
+            border: none;
+            border-radious: 20px;
+            background: none;
+            font-family: 'Montserrat';
+            font-size: 18px;
+        }
+
+        a {
+            color: #000000;
+        }
+
+        a, button {
+            text-decoration: underline dotted;
+            curdor: pointer;
+        }
+
+        button:active {
+            background: #76EDED;;
+            
+        }
+        a:active {
+            background: #76EDED;;
+            
+        }
     }
 `;
