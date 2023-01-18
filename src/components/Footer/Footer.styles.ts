3f09927123f202366bbaec67abddd101efd16553
Mari Vaypan <avemari98@gmail.com>
2023-01-19 02:19:17 +0300

    add width to component tag and image

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.footer`
+    width: 5vw;
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    img {
+        width: 2vw;
+    }
 `;
