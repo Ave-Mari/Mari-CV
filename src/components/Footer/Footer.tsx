3edf1a360963548493a4eb7c695b32fe7a59e419
Mari Vaypan <avemari98@gmail.com>
2023-01-19 02:06:51 +0300

    add github link and date to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+//image
+import Github from './../../assets/github.svg';
+//styles
+import { Wrapper } from './Footer.styles'
 
 export default function Footer() {
   return (
-    <footer>
-        
-    </footer>
+    <Wrapper>
+        <a href='https://github.com/Ave-Mari/Mari-CV' target="_blank">
+    <img src={Github} alt={'link to github'}></img>
+    </a>   
+    <p>2023</p> 
+    </Wrapper>
   )
 }
