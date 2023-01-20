f56383dee8103b4f858148b017dfc3ee0962aaba
Mari Vaypan <avemari98@gmail.com>
2023-01-20 19:04:20 +0300

    add basic style to popup component

---
+++ src/components/Popup/Popup.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    position: fixed;
+    top: 18px;
+    background: #00E1D3;
+    color: #fff;
+    padding: 6px 22px;
+    transition: cubic-bezier(.86,0,.07,1);
+`;
