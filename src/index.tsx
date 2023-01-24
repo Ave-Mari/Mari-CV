997055cf36c723ab6727890b57eb25b7b03a743e
Mari Vaypan <avemari98@gmail.com>
2023-01-24 21:02:59 +0300

    add favicon package

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Favicon from 'react-favicon'
 import App from './App';
 
 const client = new QueryClient();
