d97af2ab80b9cc8a28f5143fcaa05521debc685a
Mari Vaypan <avemari98@gmail.com>
2023-01-25 01:12:13 +0300

    add width and font size to popup

--- src/components/Popup/Popup.styles.ts
+++ src/components/Popup/Popup.styles.ts
@@ -5,9 +5,11 @@ export const Wrapper = styled.div.attrs({
     className: 'animate__backInDown animate__backOutUp'
 })`
     position: fixed;
+    width: 37vw;
     top: 18px;
     background: #00E1D3;
     color: #fff;
+    font-size: 2vw;
     padding: 6px 22px;
     text-align: center;
     left: 39%;
