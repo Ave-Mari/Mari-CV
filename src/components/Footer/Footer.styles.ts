dc4871ca0b355cc7b5bdc153fcc9c124e250b6b4
Mari Vaypan <avemari98@gmail.com>
2023-01-31 17:40:34 +0300

    add styles to footer

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,16 +1,39 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.footer`
-    width: 5vw;
+    width: 79px;
     display: flex;
     align-items: center;
     justify-content: space-between;
 
     img {
-        width: 2vw;
+        width: 2.5vw;
     }
 
     p {
         padding: 0;
     }
+
+    @media only screen and (max-width: 1024px) {
+        img {
+            width: 3vw;
+        }
+    
+        width: 82px;
+      } 
+    @media only screen and (max-width: 768px) {
+        img {
+            width: 33px;
+        }
+    
+        width: 77px;
+      } 
+
+      @media only screen and (max-width: 480px) {
+        img {
+            width: 34px;
+        }
+    
+        width: 82px;
+      }
 `;
