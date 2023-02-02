32e8a2cc36b2c2048804331177fe52874cb0a1ef
Mari Vaypan <avemari98@gmail.com>
2023-02-02 23:15:45 +0300

    add styles to small screens

--- src/components/Popup/Popup.styles.ts
+++ src/components/Popup/Popup.styles.ts
@@ -8,9 +8,20 @@ export const Wrapper = styled.div.attrs({
     top: 18px;
     background: #00E1D3;
     color: #fff;
-    font-size: 1.6vw;
+    font-size: 17px;
     padding: 6px 22px;
     text-align: center;
     right: 35%;
     left: 35%;
+
+    @media only screen and (max-width: 768px) {
+        font-size: 2.4vw;
+        right: 25%;
+        left: 25%;
+    }
+    @media only screen and (max-width: 480px) {
+        font-size: 2.9vw;
+        right: 19%;
+        left: 19%;
+    }
 `;
