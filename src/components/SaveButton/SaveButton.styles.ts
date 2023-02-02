90fc3e0e8363c536462d20b3e95e1ee2d27b29df
Mari Vaypan <avemari98@gmail.com>
2023-02-03 00:02:36 +0300

    add basic styles to save button

---
+++ src/components/SaveButton/SaveButton.styles.ts
@@ -0,0 +1,16 @@
+import styled from'styled-components';
+
+export const Wrapper = styled.button`
+    width: 42px;
+    height: 42px;
+    border: none;
+    border-radius: 50%;
+    background: #1082e6;
+    position: fixed;
+    right: 2%;
+    cursor: pointer;
+
+    &:hover {
+        background: #096ac0;
+    }
+`;
