909e1c084c3a64614790a5b23c51d6737cf3bd9d
Mari Vaypan <avemari98@gmail.com>
2023-02-10 02:34:17 +0300

    replace cv downloading to google drive link

--- src/components/SaveButton/SaveButton.tsx
+++ src/components/SaveButton/SaveButton.tsx
@@ -7,20 +7,13 @@ import { Wrapper } from './SaveButton.styles'
 
 const SaveButton: React.FC= () => {
 
-    const saveHandler = () => {
-        fetch('CV_Frontend-developer(MariVaypan).pdf').then(response => {
-            response.blob().then(blob => {
-                const fileURL = window.URL.createObjectURL(blob);
-                let alink = document.createElement('a');
-                alink.href = fileURL;
-                alink.download = 'CV_Frontend-developer(MariVaypan).pdf';
-                alink.click();
-            })
-        })
-    }
     return (
-        <Wrapper onClick={saveHandler}>
+        <Wrapper>
+            <a href="https://drive.google.com/file/d/1Z22dNQgYbP7IeZR9B50ynE1-9RRqiebQ/view"
+            target="_blank"
+            >
             <img src={Save} alt='save'></img>
+            </a>
         </Wrapper>
     )
 }
