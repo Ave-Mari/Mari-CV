31c8e9bbd30d884b86fc989eb816d1f6b4e84e23
Mari Vaypan <avemari98@gmail.com>
2023-06-08 20:27:31 +0300

    add style to main section

--- src/components/MainSection/MainSection.styles.ts
+++ src/components/MainSection/MainSection.styles.ts
@@ -6,28 +6,41 @@ export const Wrapper = styled.section`
     border-radius: 17px;
     margin-bottom: 25px;
     padding: 30px;
-    
 
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    justify-content: space-between;
 
     h1 {
         font-weight: 700;
-        font-size: 2.5vw;
+        font-size: 2.1vw;
+        margin-top: 0px;
+        margin-bottom: 10px;
+    }
+
+    h2 {
+        font-size: 2vw;
+        font-weight: 600;
         margin-top: 0px;
         margin-bottom: 10px;
     }
 
     h3 {
         font-weight: 500;
-        font-size: 1.5vw;
+        font-size: 1.2vw;
         margin: 0px;
 
     }
 
     .avatar {
         width: 240px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .basic-info {
+        display: flex;
+        flex-direction: column;
     }
 
     @media only screen and (max-width: 1024px) {
