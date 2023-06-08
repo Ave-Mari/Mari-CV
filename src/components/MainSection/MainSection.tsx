efe2349b8a721940a9aa0bdc5b62fd9642d0a668
Mari Vaypan <avemari98@gmail.com>
2023-06-08 20:26:51 +0300

    make two blocks on main section

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,22 +1,32 @@
 import * as React from 'react';
+//components
+import Table from './../Table/Table'
 //image
 import Avatar from "./../../assets/cv-pic.jpg";
 //types
 type Props = {
     name: string,
     position: string,
+    title: string
 }
 //styles
 import { Wrapper } from './MainSection.styles'
 
 
 
-const MainSection: React.FC<Props> = ({name, position }) => {
+
+const MainSection: React.FC<Props> = ({name, position, title }) => {
   return (
     <Wrapper>
-         <img src={Avatar} alt='CV Picture' className='avatar'/>
+      <div className="avatar"><img src={Avatar} alt='CV Picture' className='avatar'/>
          <h1>{name}</h1>
         <h3>{position}</h3>
+        </div>
+        <div className="basic-info">
+          <h2>{title}</h2>
+          <Table />
+        </div>
+         
     </Wrapper>
   )
 }
