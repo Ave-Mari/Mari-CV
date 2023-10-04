23de7c0488006f4aefebd899adac0a7361f12752
Mari Vaypane <avemari98@gmail.com>
2023-10-05 01:43:42 +0300

    add new skills to list

--- src/skills-data.tsx
+++ src/skills-data.tsx
@@ -1,3 +1,18 @@
 let skills: Array<string>;
 
-export default skills = ['React.js', 'TypeScript', 'JavaScript', 'HTML', 'CSS', 'Styled-Components', 'SASS', 'Webpack', 'Vite', 'Node.js', 'SQL'];
+export default skills = [
+  "JavaScript",
+  "TypeScript",
+  "React.js",
+  "Redux",
+  "Redux-Toolkit",
+  "Vue.js",
+  "HTML",
+  "CSS",
+  "Styled-Components",
+  "SASS",
+  "Webpack",
+  "Vite",
+  "Node.js",
+  "PostgreSQL",
+];
